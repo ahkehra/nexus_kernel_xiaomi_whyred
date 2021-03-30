bf49802387490dd2a5a409b73a4f2e7bcd2ec717
akira-vishal <vishal.rockstar7011@gmail.com>
2021-03-30 12:58:24 +0700

    build: update scripts

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@
 #
 # Script For Building Android Kernel By akira-vishal
 #
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+WHITE='\033[0m'
+GREY='\e[0;32m'
+GREEN='\e[1;32m'
 KERNEL_DIR=$PWD
 TANGGAL=$(date +"%F-%S")
 DATE=$(date +"%m-%d-%y")
 START=$(date +"%s")
 DEVICE=WhyRed
-DEFCONFIG=whyred-oldcam_defconfig
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
+DEFCONFIG=whyred_defconfig
 #
 # use ccache
 export USE_CCACHE=1
@@ -20,35 +22,34 @@ export USE_CCACHE=1
 #ccache variables
 export CCACHE_DIR="$HOME/.ccache"
 export CC="ccache gcc"
-export CXX="ccache gcc++"
+export CXX="ccache g++"
 export PATH="/usr/lib/ccache:$PATH"
 #
+#Export variables
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export LOCALVERSION="-X3"
+export KBUILD_COMPILER_STRING=$(gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export KBUILD_BUILD_HOST="akira"
+export KBUILD_BUILD_USER="vishal"
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-#
-#Export ARCH <arm, arm64, x86, x86_64>
-export ARCH=arm64
-#
-#Export SUBARCH <arm, arm64, x86, x86_64>
-export SUBARCH=arm64
-#
-#Set kernal name
-export LOCALVERSION=-X3
-#Export Username
-export KBUILD_BUILD_USER=VISHAL
-#Export Machine name
-export KBUILD_BUILD_HOST=AKIRA
 #
 function checker() {
     if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ]
        then
-        echo -e "\e[1;32mCloning AnyKernel3\e[0m"
+        echo -e ""
+        echo -e "$CYAN- Cloning AnyKernel3$WHITE"
         cd && git clone https://github.com/akira-vishal/AnyKernel3.git AnyKernel
-        echo -e "\e[1;32mDone!\e[0m"
+        echo -e ""
+        echo -e "$GREEN- Done!$WHITE"
         cd $HOME && cd $KERNEL_DIR
         zipper
        else
-        echo -e "\e[1;32mBuild failed\e[0m"
+        echo -e ""
+        echo -e "$RED- Build Failed$WHITE"
+        
     fi
 }
 #
@@ -56,23 +57,28 @@ function zipper() {
     rm -f $HOME/AnyKernel/Image.gz*
     rm -f $HOME/AnyKernel/zImage*
     rm -f $HOME/AnyKernel/dtb*
-    echo -e "\e[1;32mTime To ZIP Up!\e[0m"
+    echo -e ""
+    echo -e "$CYAN- Time To ZIP Up!$WHITE"
     cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $HOME/AnyKernel
     cd $HOME/AnyKernel || exit 1
     zip -r9 neXus-X3_${DEVICE}-KERNEL-${TANGGAL}.zip *
     cd $HOME && cd $KERNEL_DIR
     END=$(date +"%s")
     DIFF=$(($END - $START))
-    echo -e "\e[1;32mBuild Completed Succesfully\e[0m"
-    echo -e "\e[1;32mBuild took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s).\e[0m"
+    echo -e ""
+	echo -e "$GREEN- Build Completed Succesfully$WHITE"
+	echo -e ""
+	echo -e "$GREEN- Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s).$WHITE"
+	echo -e ""
 }
 #
 function compiler() {
     rm -rf $KERNEL_DIR/out
-    echo -e "\e[1;32mBuilding Kernel\e[0m"
+    echo -e ""
+	echo -e "$CYAN- Building Kernel$WHITE"
     make clean && make mrproper O=out
     make O=out	${DEFCONFIG}
-    make O=out -j$(nproc --all) 
+    make -j$(nproc --all) O=out
 }
 compiler
 checker
